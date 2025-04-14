8d8e96cfaac028f463cc4c33756b33c6f688fc32
ales <hi@alesnovotny.cz>
2025-04-14 23:26:55 +0200

    add update path to install script

--- install.sh
+++ install.sh
@@ -1,24 +1,33 @@
 #!/usr/bin/env bash
 
-systemctl -q is-active pmxcfs-ram && {
-    echo "ERROR: pmxcfs-ram is already installed and running."
-    exit 1
-}
-
 [ "$(id -u)" -eq 0 ] || {
     echo "Must be run with root privileges"
     exit 1
 }
 
-mkdir -p /usr/local/bin/ || exit 1
-install -m 755 pmxcfs-ram.sh /usr/local/bin/pmxcfs-ram.sh || exit 1
-echo "++ installed /usr/local/bin/pmxcfs-ram.sh"
+function deploy () {
+    mkdir -p /usr/local/bin/ || exit 1
 
-install -m 644 pmxcfs-ram.service /etc/systemd/system/pmxcfs-ram.service || exit 1
-echo "++ installed /etc/systemd/system/pmxcfs-ram.service"
+    install -m 755 pmxcfs-ram.sh /usr/local/bin/pmxcfs-ram.sh || exit 1
+    echo "-> installed /usr/local/bin/pmxcfs-ram.sh"
 
-systemctl enable pmxcfs-ram.service
-echo "++ enabled pmxcfs-ram.service"
+    install -m 644 pmxcfs-ram.service /etc/systemd/system/pmxcfs-ram.service || exit 1
+    echo "-> installed /etc/systemd/system/pmxcfs-ram.service"
+}
 
-echo "## installation successful"
-echo "## now reboot to activate the service"
+function enable_service () {
+    systemctl enable pmxcfs-ram.service
+    echo "++ enabled pmxcfs-ram.service"
+}
+
+deploy
+
+systemctl -q is-active pmxcfs-ram && {
+    echo "## update successful"
+    echo "## now reboot to reload the service"
+} || {
+    enable_service
+
+    echo "## installation successful"
+    echo "## now reboot to activate the service"
+}
